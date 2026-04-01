64a38377243668d16bcdee4428344983f7cf7e7f
Prateek Aghi <82527015+prateekaghi@users.noreply.github.com>
2026-04-01 18:55:12 -0400

    Add: count-good-nodes-in-binary-tree - submission-1

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-1.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    goodNodes(root) {
+        let goodNodes=0
+
+
+        function traverse(node,max){
+            if(!node)return 
+            if(node.val>=max){
+                goodNodes++
+            }
+            traverse(node.left,Math.max(max,node.val))
+            traverse(node.right,Math.max(max,node.val))
+
+        }
+        traverse(root,root.val)
+        return goodNodes
+    }
+}
