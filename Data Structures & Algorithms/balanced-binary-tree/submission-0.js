bcdac6eedc1e433869383d6e63e0a41f5ad9b6be
Prateek Aghi <82527015+prateekaghi@users.noreply.github.com>
2026-04-06 11:34:31 -0400

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+
+        function dfs(node){
+            if(!node)return 0
+            const left= dfs(node.left)
+            if(left === -1)return -1
+
+            const right=dfs(node.right)
+            if(right===-1)return -1
+
+            if(Math.abs(left-right)>1)return -1
+
+            return 1+  Math.max(left,right)
+        }
+        return dfs(root) !==-1
+    }
+}
